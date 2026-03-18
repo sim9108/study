fc263b80b590a8e661501b909aaffbbb836de2ad
sim9108 <sim9108@naver.com>
2026-03-18 13:45:05 +0900

    expected basic example

--- level01/01.cpp
+++ level01/01.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 auto main() -> int {
 	println("Hello World C++23");
+	println("값: {}", 42);
 	return 0;
 }
---
+++ level01/02.cpp
@@ -0,0 +1,13 @@
+﻿import std;
+using namespace std;
+
+expected<int, string> divide(int a, int b) {
+    if (b == 0) return unexpected("b is 0"s);
+    return a / b;
+}
+
+int main() {
+    auto r = divide(10, 0);
+    if (r) std::println("결과: {}", *r);
+    else   std::println("오류: {}", r.error());
+}
