bcf1e5b39431921ec9823dfa1be4646491213516
sim9108 <sim9108@naver.com>
2026-03-18 17:12:28 +0900

    more code

--- level01/01.cpp
+++ level01/01.cpp
@@ -21,14 +21,11 @@ auto main() -> int {
 	println("{:#o}", 8);     // "010"
 	println("{:#b}", 5);     // "0b101
 
-	//[width][.precision]
-	println("{:010}", 42);     // "        42"
-	println("{:.5f}", 3.14);  // "3.14000"
+	//[sign][0][width][.precision]
+	println("{:010}", 42);			// "0000000042"
+	println("{:+10}", 42);			// "       +42"
+	println("{:010.5f}", 3.14);		// "0003.14000"
 	println("{:{}.{}}", 3.14, 8, 3);  // 동적 width/precision
-
-	//[sign][0][width]
-	println("{:08}", 42);   // "00000042"
-	println("{:+08}", 42);  // "+0000042"
 
 	//[L]
 	std::cout << format(locale("ko_KR"), "{:L}", 1000000) << endl;  // "1,000,000"
