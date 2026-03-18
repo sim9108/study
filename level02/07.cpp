de70e3c8a142cebfae0eb7ea34be277a27b61cb4
sim9108 <sim9108@naver.com>
2026-03-18 14:10:07 +0900

    level02 add

---
+++ level02/07.cpp
@@ -0,0 +1,18 @@
+﻿import std;
+using namespace std;
+
+expected<int, string> divide(int a, int b) {
+    if (b == 0) return unexpected("b is 0"s);
+    return a / b;
+}
+
+auto main() -> int {
+    auto r = divide(10, 0);
+    if (r) {
+        println("result: {}", *r);
+    }
+    else {
+        println("error: {}", r.error());
+    }
+    return 0;
+}
