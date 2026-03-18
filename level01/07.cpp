385c116523f4ac2919bbc03e74c7c0e92dbf9e70
sim9108 <sim9108@naver.com>
2026-03-18 20:08:04 +0900

    if constexpr concept example

---
+++ level01/07.cpp
@@ -0,0 +1,18 @@
+﻿import std;
+using namespace std;
+
+template <typename T>
+void check_type(T val) {
+    if constexpr (integral<T>) {
+        println("integral");
+    }
+    else {
+        println("not integral");
+    }
+}
+
+auto main() -> int {
+    check_type(10);
+    check_type(3.14);
+    return 0;
+}
