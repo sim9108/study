766987ef5e2f978e0cbc6dde4fbe385e6e7886a7
sim9108 <sim9108@naver.com>
2026-03-18 17:07:17 +0900

    more code

--- level01/04.cpp
+++ level01/04.cpp
@@ -5,8 +5,12 @@ auto main() -> int {
     vector keys = { 1, 2, 3 };
     vector values = { "one", "two", "three" };
 
+    println("keys:{}", keys);
+    println("values:{}", values);
+
     for (auto [k, v] : views::zip(keys, values)) {
-        std::println("{} → {}", k, v);
+        println("{} → {}", k, v);
     }
+
     return 0;
 }
