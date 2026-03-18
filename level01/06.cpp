b011c50a025e0249ed652ad5ab4152ca3c018e92
sim9108 <sim9108@naver.com>
2026-03-18 14:09:06 +0900

    more code

--- level01/06.cpp
+++ level01/06.cpp
@@ -1,12 +1,10 @@
 ﻿import std;
+using namespace std;
 
 auto main() -> int {
-    using namespace std;
-    using namespace std::ranges;
+    vector v = { 1, 2, 3, 4, 5, 6, 7 };
 
-    auto v = views::iota(1, 6)          // 1,2,3,4,5
-        | views::transform([](int x) { return x * x; })
-        | to<std::vector>();  // {1,4,9,16,25}
-
-    print("{} ", v);
+    auto chunk = v | std::views::chunk(2);
+    println("{}", chunk);
+    return 0;
 }
