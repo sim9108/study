628ffe5ce9817e182409fcafc8ead908e011060c
sim9108 <sim9108@naver.com>
2026-03-19 08:52:59 +0900

    formatter example front

--- level01/005.cpp
+++ level01/005.cpp
@@ -1,24 +1,10 @@
 ﻿import std;
 using namespace std;
 
-namespace NS1 {
-    struct Point { int x, y; };
-}
-namespace std {
-    using NS1::Point;
-    template <>
-    struct formatter<Point> {
-        constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
-
-        auto format(const Point& p, format_context& ctx) const {
-            return std::format_to(ctx.out(), "({}, {})", p.x, p.y);
-        }
-    };
-}
-
 auto main() -> int {
-    NS1::Point p{ 10, 20 };
-    println("{}", p); //(10, 20)
+    for (vector v = { 1, 2, 3, 4, 5 }; auto x : v) {
+        println("{}", x);
+    }
+
     return 0;
 }
-
