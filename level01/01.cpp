9b520af473076b80273081e666f7b76ff4258ad1
sim9108 <sim9108@naver.com>
2026-03-18 17:13:37 +0900

    more code

--- level01/01.cpp
+++ level01/01.cpp
@@ -16,15 +16,13 @@ auto main() -> int {
 	println("{: }", 42); // 양수는 공백
 	println("{:-}", 42); // 음수만 부호 (기본값)
 
-	//[#]
-	println("{:#x}", 255);   // "0xff"
-	println("{:#o}", 8);     // "010"
-	println("{:#b}", 5);     // "0b101
-
-	//[sign][0][width][.precision]
-	println("{:010}", 42);			// "0000000042"
-	println("{:+10}", 42);			// "       +42"
-	println("{:010.5f}", 3.14);		// "0003.14000"
+	//[sign][#][0][width][.precision]
+	println("{:#x}", 255);			  // "0xff"
+	println("{:#o}", 8);			  // "010"
+	println("{:#b}", 5);			  // "0b101	
+	println("{:010}", 42);			  // "0000000042"
+	println("{:+10}", 42);			  // "       +42"
+	println("{:010.5f}", 3.14);		  // "0003.14000"
 	println("{:{}.{}}", 3.14, 8, 3);  // 동적 width/precision
 
 	//[L]
