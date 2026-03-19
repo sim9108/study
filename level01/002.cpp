5c1716eca44ce1b55e0fd7cdd249893fa4a76b60
sim9108 <sim9108@naver.com>
2026-03-19 15:13:17 +0900

    [type] specifier add

--- level01/002.cpp
+++ level01/002.cpp
@@ -9,12 +9,15 @@ auto main() -> int {
 	println("{:-}", 42);		// 음수만 부호 (기본값)
 
 	//[sign][#][0][width][.precision][type]
+	//[type]: d b/B o x/X f/F e/E a/A c s p ? #
 	println("{:#x}", 255);			  // "0xff"
 	println("{:#o}", 8);			  // "010"
 	println("{:#b}", 5);			  // "0b101	
 	println("{:010}", 42);			  // "0000000042"
 	println("{:+10}", 42);			  // "       +42"
 	println("{:010.5f}", 3.14);		  // "0003.14000"
+	println("{:08d}", 255);			  // "00000255"
+	println("{:#010x}", 255);		  // "0x000000ff"
 	println("{:{}.{}}", 3.14, 8, 3);  // 동적 width/precision
 	return 0;
 }
