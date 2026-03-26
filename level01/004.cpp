bdfe793c61df2a5227782ef3a6db4303f3b64c01
sim9108 <sim9108@naver.com>
2026-03-26 15:54:32 +0900

    void * format

--- level01/004.cpp
+++ level01/004.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 
 auto main() -> int {
-    println("{} {}", 123, 456);                      //"123 456"
-    println("{}", formatted_size("{} {}", 123, 456));//"7"
-    println("{}", nullptr);                          //0x0
+    println("{} {}", 123, 456);                      // "123 456"
+    println("{}", formatted_size("{} {}", 123, 456));// "7"
+    println("{}", nullptr);                          // "0x0"
+
+    int a{ 10 };
+    void* p = &a;
+    println("{0:P} {0:p}", p);           // "0X6FF7EC 0x6ff7ec"
     return 0;
 }
