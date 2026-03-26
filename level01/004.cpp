bf7a3b8d0d017be43c540d9c048a36f5befcf5aa
sim9108 <sim9108@naver.com>
2026-03-26 15:51:39 +0900

    nullptr formatting

--- level01/004.cpp
+++ level01/004.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 
 auto main() -> int {
-    println("{} {}", 123, 456);         //"123 456"
+    println("{} {}", 123, 456);                      //"123 456"
     println("{}", formatted_size("{} {}", 123, 456));//"7"
+    println("{}", nullptr);                          //0x0
     return 0;
 }
