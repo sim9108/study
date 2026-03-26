3e7a5b878e880d19f289798291544944991faec9
sim9108 <sim9108@naver.com>
2026-03-26 12:17:46 +0900

    pair/tuple format-spec

--- level01/003.cpp
+++ level01/003.cpp
@@ -5,8 +5,10 @@ auto main() -> int {
     //{[arg-id]:[[fill]align][sign][#][0][width][.precision][L][type]}
     //:[[fill]align][width]
     vector data{ 1, 2, 3 };
-    println("{}", data);            // "[1, 2, 3]"
-    println("{::>5}", data);        // "[    1,     2,     3]"
-    println("{}", pair{ 1, "hi" }); // "(1, hi)"
-	return 0;
+    println("{}", data);              // "[1, 2, 3]"
+    println("{::>5}", data);          // "[    1,     2,     3]"
+    println("{}", pair{ 1, "hi" });   // "(1, hi)"
+    println("{:m}", pair{ 1, "hi" }); // "1: "hi""
+    println("{:n}", pair{ 1, "hi" }); // "1, "hi""
+    return 0;
 }
