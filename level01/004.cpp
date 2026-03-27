0262c55af2a45a82d0f40fd8795e93eecffaa8d4
sim9108 <sim9108@naver.com>
2026-03-27 16:05:52 +0900

    map range spec

--- level01/004.cpp
+++ level01/004.cpp
@@ -20,5 +20,12 @@ auto main() -> int {
     println("{}", pair{ 1, "hi" });    // "(1, hi)"
     println("{:m}", pair{ 1, "hi" });  // "1: "hi""
     println("{:n}", pair{ 1, "hi" });  // "1, "hi""
+
+    map<string, int> map;
+    map["math"] = 10;
+    map["lang"] = 100;
+    println("{}", map);               // "{"lang": 100, "math": 10}" 
+    println("{:n}", map);             // ""lang": 100, "math": 10"
+
     return 0;
 }
