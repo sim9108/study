1205307b513456e35c6b6f1bdafb477863b9141d
sim9108 <sim9108@naver.com>
2026-03-27 21:50:19 +0900

    tupel format spec

--- level01/004.cpp
+++ level01/004.cpp
@@ -17,15 +17,5 @@ auto main() -> int {
     println("{:s}", sdata);            // "ABC  "<new line>
     println("{:?s}", sdata);           // ""ABC\t\n""
 
-    println("{}", pair{ 1, "hi" });    // "(1, hi)"
-    println("{:m}", pair{ 1, "hi" });  // "1: "hi""
-    println("{:n}", pair{ 1, "hi" });  // "1, "hi""
-
-    map<string, int> map;
-    map["math"] = 10;
-    map["lang"] = 100;
-    println("{}", map);               // "{"lang": 100, "math": 10}" 
-    println("{:n}", map);             // ""lang": 100, "math": 10"
-
     return 0;
 }
--- level01/005.cpp
+++ level01/005.cpp
@@ -1,7 +1,20 @@
 ﻿import std;
 using namespace std;
 
-
 auto main() -> int {
+    // [[tuple-fill]align][width][tuple-type] =>tuple-format-spec
+    // [[tuple-fill]align] tuple-fill로 { } : 불가능
+    // [tuple-type]: m n
+
+    println("{}", pair{ 1, "hi" });    // "(1, hi)"
+    println("{:m}", pair{ 1, "hi" });  // "1: "hi""
+    println("{:n}", pair{ 1, "hi" });  // "1, "hi""
+
+    map<string, int> map;
+    map["math"] = 10;
+    map["lang"] = 100;
+    println("{}", map);               // "{"lang": 100, "math": 10}" 
+    println("{:n}", map);             // ""lang": 100, "math": 10"
+
     return 0;
 }
