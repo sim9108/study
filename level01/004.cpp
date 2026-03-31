838cece578d0ac977de3b3e5b2244886ba32a252
sim9108 <sim9108@naver.com>
2026-03-31 14:04:00 +0900

    seperated for tuple format spec

--- level01/004.cpp
+++ level01/004.cpp
@@ -33,12 +33,5 @@ auto main() -> int {
     println("{}", m);                 // "[(1, "a"), (2, "b")]"
     println("{:m}", m);               // "{1: "a", 2: "b"}"
     println("{:nm}", m);              // "1: "a", 2: "b""
-
-    // {[arg-id]:[[tuple-fill]align][width][tuple-type]
-    // [[tuple-fill]align] tuple-fill로 { } : 불가능
-    // [tuple-type]: m n
-    println("{}", pair{ 1, "hi" });    // "(1, hi)"
-    println("{:m}", pair{ 1, "hi" });  // "1: "hi""
-    println("{:n}", pair{ 1, "hi" });  // "1, "hi""
     return 0;
 }
--- level01/005.cpp
+++ level01/005.cpp
@@ -2,6 +2,11 @@
 using namespace std;
 
 auto main() -> int {
-
+    // {[arg-id]:[[tuple-fill]align][width][tuple-type]
+    // [[tuple-fill]align] tuple-fill로 { } : 불가능
+    // [tuple-type]: m n
+    println("{}", pair{ 1, "hi" });    // "(1, hi)"
+    println("{:m}", pair{ 1, "hi" });  // "1: "hi""
+    println("{:n}", pair{ 1, "hi" });  // "1, "hi""
     return 0;
 }
