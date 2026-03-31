68b81598d0f35c93c38c59f36d6c8ed9c76927c2
sim9108 <sim9108@naver.com>
2026-03-31 14:19:34 +0900

    stacktrace format spec

--- level01/006.cpp
+++ level01/006.cpp
@@ -1,8 +1,24 @@
 ﻿import std;
 using namespace std;
 
-//chrono-format-spec
+//{[arg-id]:[[fill]align][width]}
 
+void call3() {
+    println("{}", __func__);
+    auto st = stacktrace::current();
+    println("{}", st);
+}
+
+void call2() {
+    println("{}", __func__);
+    call3();
+}
+
+void call1() {
+    println("{}", __func__);
+    call2();
+}
 auto main() -> int {
+    call1();
     return 0;
 }
