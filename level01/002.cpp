f8b8ed2a8a3b39fe5c3c02e8e20966d6891d107c
sim9108 <sim9108@naver.com>
2026-04-01 18:28:35 +0900

    more classify

--- level01/002.cpp
+++ level01/002.cpp
@@ -10,13 +10,13 @@ auto main() -> int {
 
 	//[sign][#][0][width][.precision][type]
 	//[type]: a/A b/B c d e/E f/F g/G o p/P s x/X ?
+	//[#]
 	println("{0:#b} {0:#d} {0:#o} {0:#x}", 42);  // "0b101010 42 052 0x2a"
-
 	println("{0:#} {0}", 123.0);      // "123. 123"
+	//[align] vs [0]
 	println("{:+#10X}", 'x');         // "     +0X78"
 	println("{:+#010X}", 'x');        // "+0X0000078"
 	println("{:_>+#10X}", 'x');       // "_____+0X78"
-	println("{:#010x}", 255);		  // "0x000000ff"	
 
 	//정밀도
 	println("{:010.5f}", 3.14);		  // "0003.14000"
