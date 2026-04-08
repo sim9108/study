7daff20ad0d1ab36851e2ae9f1015bf54bc19e59
sim9108 <sim9108@naver.com>
2026-04-09 07:47:20 +0900

    generic pathname format spec

--- level002/001.cpp
+++ level002/001.cpp
@@ -3,6 +3,15 @@ using namespace std;
 using namespace std::filesystem;
 
 auto main() -> int {
+	// pathname:[root-name][root-directory][relative-path]
+	// root-directory:
+	//		directory - separator
+	// directory-separator:
+	//		preferred-separator [directory-separator]
+	//		fallback-separator [directory-separator]
+	// relative-path:
+	//		filename
+	//		filename directory-separator relative-path
 	const path p = path("C:\\AVerMedia\\AssistCentralPro\\styles\\qwindowsvistastyle.dll");
 	cout <<"full path:" << p << endl;
 	cout << "root-name:" << p.root_name()<<endl;
