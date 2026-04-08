af44b0bffb5dbb8b723eca478d81be32010f69f0
sim9108 <sim9108@naver.com>
2026-04-09 07:39:07 +0900

    filesystem example

---
+++ level002/001.cpp
@@ -0,0 +1,19 @@
+﻿import std;
+using namespace std;
+using namespace std::filesystem;
+
+auto main() -> int {
+	const path p = path("C:\\AVerMedia\\AssistCentralPro\\styles\\qwindowsvistastyle.dll");
+	cout <<"full path:" << p << endl;
+	cout << "root-name:" << p.root_name()<<endl;
+	cout << "root-directory:" << p.root_directory()<<endl;
+	cout << "root-path:" << p.root_path() << endl;
+	cout << "relative-path:\n";
+	for (auto const& subpath : p.relative_path()) {
+		cout << subpath << endl;
+	}
+	cout << "filename:" << p.filename() << endl;
+	cout << "stem:" << p.stem() << endl;
+	cout << "extension:" << p.extension() << endl;
+	return 0;
+}
