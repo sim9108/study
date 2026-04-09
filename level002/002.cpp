e265fc3b96a03f4589b77173c0f33fee4d0239fe
sim9108 <sim9108@naver.com>
2026-04-09 20:05:35 +0900

    directory_entry and path association

---
+++ level002/002.cpp
@@ -0,0 +1,13 @@
+import std;
+using namespace std;
+using namespace std::filesystem;
+
+auto main() -> int {
+	auto p = path("C:\\test.mp4");
+	directory_entry entry{ p };
+	cout << entry << endl;
+	for (const directory_entry& item : directory_iterator("C:\\")) {
+		cout << item << endl;
+	}
+	return 0;
+}
