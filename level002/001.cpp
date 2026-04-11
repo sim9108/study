977f2c45f652b29d43bfe073b3395fed638b80f1
sim9108 <sim9108@naver.com>
2026-04-11 16:42:17 +0900

    generic pathname format spec fixed

--- level002/001.cpp
+++ level002/001.cpp
@@ -3,15 +3,17 @@ using namespace std;
 using namespace std::filesystem;
 
 void view_pathname_spec(const path& p) {
+	// pathname:[root-path][relative-path]
 	// pathname:[root-name][root-directory][relative-path]
 	// root-directory:
-	//		directory - separator
+	//		directory-separator
 	// directory-separator:
-	//		preferred-separator [directory-separator]
-	//		fallback-separator [directory-separator]
+	//		preferred-separator[directory-separator]
+	//		fallback-separator[directory-separator]
 	// relative-path:
 	//		filename
-	//		filename directory-separator relative-path
+	//		filename directory-separator[relative-path]
+	//      empty path
 	cout << "full path:" << p << endl;
 	cout << "\troot-name:" << p.root_name() << endl;
 	cout << "\troot-directory:" << p.root_directory() << endl;
