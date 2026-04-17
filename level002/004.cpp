bf5751c6120fd1328b19b7186784a87e0d78e557
sim9108 <sim9108@naver.com>
2026-04-18 00:05:09 +0900

    simple pathname resolution example

--- level002/004.cpp
+++ level002/004.cpp
@@ -11,27 +11,27 @@ void lexicall_relative(path const& lhs, const path& base) {
 	wcout << setw(40) << left << ss.str() << ";// path(" << app << L")" << endl;
 }
 
-auto main() -> int {	
+auto main() -> int {
 	// [root-name][root-directory][relative-path]
 
 	// 계산 불가능:path() 반환
 	// root_name() != base.root_name() 
 	// !has_root_directory() && base.has_root_directory()
 	// is_absolute() != base.is_absolute() 	
-	lexicall_relative("c:a/b", "d:a/b/c");		
+	lexicall_relative("c:a/b", "d:a/b/c");
 	lexicall_relative("c:a", "c:/a");
-		
+
 	// auto [a, b] = mismatch(begin(), end(), base.begin(), base.end());
-	
+
 	// a==end() && b==base.end() : path(".") 반환
 	// path()/path("..") 연산을 반복할 횟수 n 계산: "", ".", ".."
-	// n<0: path() 반환	
+	// n < 0: path() 반환	
 	// n==0 && (a==end() || a->empty()): path(".") 반환	
 	// path()/path("..")/path("..") n반복/path([a, end()]) 반환	
-	lexicall_relative("a/b", "a/b");	
+	lexicall_relative("a/b", "a/b");
 	lexicall_relative("a/d/e", "a/b/c/.././f");
 	lexicall_relative("a/d/e", "a/../");
 	lexicall_relative("a/", "a/./");
-	lexicall_relative("a/d/e", "a/b/e/f");	
+	lexicall_relative("a/d/e", "a/b/e/f");
 	return 0;
 }
---
+++ level002/005.cpp
@@ -0,0 +1,38 @@
+﻿import std;
+using namespace std;
+using namespace std::filesystem;
+
+void f_status(const file_status& fst) {
+	println("type:{:d} perm:{:#04X}",
+		to_underlying(fst.type()), to_underlying(fst.permissions())
+	);
+}
+
+void desc(path const& p) {
+	error_code ec1{}, ec2{};
+	file_status f1 = status(p, ec1);		
+	 file_status f2 = symlink_status(p, ec2); // pathname resolution	
+	f_status(f1);
+	f_status(f2);
+
+	if (ec1) {
+		println("error1:{}", ec1.message());
+	}
+	if (ec2) {
+		println("error2:{}", ec2.message());
+	}
+}
+
+auto main() -> int {	
+	// [root-name][root-directory][relative-path]
+	desc("mydir/readme.txt");
+	desc("mydir/sub1.lnk");
+
+
+	error_code ec{};
+	directory_entry entry{ "mydir/readme.txt" };
+	println("file size:{}", entry.file_size());
+	println("last_write_time:{}", entry.last_write_time());
+
+	return 0;
+}
