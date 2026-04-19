22543775975eaf25f46b59f905695d0e3943dc2d
sim9108 <sim9108@naver.com>
2026-04-19 20:52:38 +0900

    recursive directory iterator

---
+++ level002/008.cpp
@@ -0,0 +1,25 @@
+﻿import std;
+using namespace std;
+using namespace std::filesystem;
+
+auto main() -> int {
+	using enum directory_options;
+	error_code ec;
+	recursive_directory_iterator files{
+		path(".")/"mydir"/"project",
+		skip_permission_denied | follow_directory_symlink,
+		ec
+	};
+
+	for (auto it = files; it != default_sentinel; ++it) {
+		if (it->is_directory() && it->path().filename() == "skip") {
+			it.disable_recursion_pending();
+			continue;
+		}
+
+		println("path:{} depth:{}", it->path().string<char>(), it.depth());
+	}
+
+	if (ec) println("error:{}", ec.message());
+	return 0;
+}
