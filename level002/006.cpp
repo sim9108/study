588cf10ec2b2282c8710bc2c1c4d04618b73b988
sim9108 <sim9108@naver.com>
2026-04-19 13:11:22 +0900

    directory_iterator example

---
+++ level002/006.cpp
@@ -0,0 +1,18 @@
+﻿import std;
+using namespace std;
+using namespace std::filesystem;
+using namespace ranges;
+
+auto main() -> int {
+	error_code ec;
+	directory_iterator files{ ".",directory_options::skip_permission_denied,ec };
+
+	auto range_files = files 
+		| views::filter([](directory_entry const& f) {return f.is_regular_file(); })
+		| views::transform([](directory_entry const& f) {return f.path().generic_string<char>(); })
+		| to<vector<string>>();
+	println("{}", range_files);
+
+	if (ec) cout << ec.message() << endl;
+	return 0;
+}
---
+++ level002/007.cpp
@@ -0,0 +1,21 @@
+﻿import std;
+using namespace std;
+using namespace std::filesystem;
+using namespace ranges;
+
+auto main() -> int {
+	error_code ec;
+	directory_iterator files{ ".",directory_options::skip_permission_denied,ec };
+
+	auto iter = ranges::find(
+		directory_iterator{ ".",directory_options::skip_permission_denied,ec },
+		default_sentinel,
+		path(".") / "ConsoleApplication3.cpp",
+		&directory_entry::path
+	);
+
+	println("Find:{:d}", iter != default_sentinel);
+
+	if (ec) cout << ec.message() << endl;
+	return 0;
+}
