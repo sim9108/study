d6b9f042f2bf3186b840b2de81782f045dcbb5d5
sim9108 <sim9108@naver.com>
2026-04-21 15:09:58 +0900

    filesystem::copy example

---
+++ level002/009.cpp
@@ -0,0 +1,22 @@
+﻿import std;
+using namespace std;
+using namespace std::filesystem;
+
+auto main() -> int {
+	error_code ec;
+	do {
+		using enum copy_options;
+		path base = path(".") / "mydir";
+		path src = base / "project";
+		path target = base / "newdir";
+
+		remove_all(target);
+
+		copy_options opt = recursive | skip_symlinks;
+		//copy_options opt = recursive | directories_only;
+		copy(src, target, opt, ec);
+		if (ec) break;
+	} while (false);
+	if (ec) println("error:{}", ec.message());
+	return 0;
+}
