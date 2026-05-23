c35ed40d68ac1a048beb7aa318a01b414cf43295
sim9108 <sim9108@naver.com>
2026-05-23 21:29:40 +0900

    miner fixed

--- level003/007.cpp
+++ level003/007.cpp
@@ -26,24 +26,6 @@ auto main() -> int {
 }
 
 /*
-// controlled sequence vs associated sequence
-// 3 pointer: xbeg, xnext, xend 
-// xbeg==xnext==xend==nullptr 허용(unbuffered stream 객체 구현 가능)
-// 
-// [output] 3 pointer: pbase(), pptr(), epptr();
-// [input]  3 pointer: eback(), gptr(), egptr();
-// 
-// [output] xnext < xend: write position is available, otherwise overflow;  
-// [output] pending sequence:[pbase(),pptr)
-// [output] consumed sequence:associated sequence로 전송 완료된 sequence
-// 
-// [input]  xnext < xend: read position is available, otherwise underflow 
-// [input]  xbeg < xnext: putback position is available, otherwise pbackfail    
-//  
-// 
-// [input] backup sequence/consume sequence:[eback(), gptr())
-// [input] pending sequence:[gptr(), egptr()) + sequence of characters read from the input sequence
-
 template<class charT, class traits = char_traits<charT>>
 class basic_streambuf {
 public:
@@ -52,23 +34,22 @@ public:
     using pos_type = traits::pos_type;
     using off_type = traits::off_type;
     using traits_type = traits;
-
 public:
     // 31.6.3.3.4, putback
     int_type sputbackc(char_type c);
     // if putback postion is available && traits::eq(c, gptr()[-1])
     //          --gptr(),returns traits::to_int_type(*gptr());
-    // otherwise 
+    // otherwise
     //          pbackfail(traits::to_int_type(c));
     int_type sungetc();
     // if putback postion is available
-    //          --gptr(),returns traits::to_int_type(*gptr()); 
-    // otherwise 
+    //          --gptr(),returns traits::to_int_type(*gptr());
+    // otherwise
     //          pbackfail();
 protected:
     virtual int_type pbackfail(int_type c = traits::eof());
     // if putback postion is not available
-    //          버퍼밀기 / 재할당 / 파일 포인터 역이동 / give up 
+    //          버퍼밀기 / 재할당 / 파일 포인터 역이동 / give up
     // if putback postion is not available
     //          return traits::eof();
     // if traits::eq_int_type(c, traits::eof())
@@ -76,75 +57,6 @@ protected:
     // if !traits::eq_int_type(c, traits::eof())
     //          gbump(-1),*gptr()=c, return traits_type::not_eof(c);
 
-public:
-    // 31.6.3.3.3, get area
-    streamsize in_avail();  
-    // if read position is available 
-    //      returns egptr() - gptr();
-    // otherwise 
-    //      return showmanyc();
-    
-    int_type sgetc();       
-    // if read position is not available 
-    //      return underflow();
-    // otherwise
-    //  traits::to_int_type(*gptr());
-
-    int_type snextc();      
-    // int_type c=sbumpc();
-    // if eq_int_type(c, traits::eof())
-    //      return traits::eof();
-    // otherwise
-    //      return sgetc();
-
-    int_type sbumpc();  
-    // if read position is not available
-    //      return uflow();
-    // otherwise
-    //      int_type ret = traits::to_int_type(*gptr()); 
-    //      gptr++; 
-    //      return ret;   
-
-    streamsize sgetn(char_type* s, streamsize n); 
-    // return xsgetn(s, n);
-
-protected:
-    // 31.6.3.4.2, get area access[input]
-    char_type* eback() const;
-    char_type* gptr() const;
-    char_type* egptr() const;
-    void       gbump(int n);
-    // gptr() += n;
-    void       setg(char_type* gbeg, char_type* gnext, char_type* gend);
-
-    // 31.6.3.5.3, get area
-    virtual streamsize showmanyc();
-    // 양수: 즉시 읽어낼 수 있는 문자수
-    //   0: blocking할 수 있지만 여전히 읽어낼 문자가 있음.
-    //  -1: 읽어낼 문자가 없음
-    virtual int_type underflow();
-    // input seqence assign from pending sequence
-    // usual backup condition
-    // return traits::to_int_type(c), where c is the first character of the pending sequence.
-    // peek mode
-    virtual int_type uflow();
-    // int_type ret = underflow(); gbump(1); return ret; 
-
-    virtual streamsize xsgetn(char_type* s, streamsize n);
-    // streamsize tcnt{};
-    // while(n>0){
-    //  if(gptr() >= egptr()) {
-    //   if(underflow() == traits_type::eof()) break;
-    //  }
-    //  streamsize count = min(egptr() - gptr(),n);
-    //  memcpy(s,gptr(),count);
-    //  gbump(count);
-    //  tcnt +=count;
-    //  n -=count
-    //  s +=count;
-    // }
-    // return tcnt; 
-    
 public:
     // 31.6.3.3.5, put area
     int_type sputc(char_type c);
@@ -171,10 +83,10 @@ protected:
     // streamsize tcnt{};
     // while(n>0){
     //  if(pptr() >= epptr()) {
-    //   if(overflow(traits_type::to_int_type(*s)) == traits_type::eof()) break;
-    //   --n;
-    //   ++tcnt;
-    //   s++;
+    //    if(overflow(traits_type::to_int_type(*s)) == traits_type::eof()) break;
+    //    --n;
+    //    ++tcnt;
+    //    s++;
     //  }
     //  streamsize count = min(epptr() - pptr(),n);
     //  memcpy(pptr(),s,count);
@@ -183,14 +95,95 @@ protected:
     //  n -=count
     //  s +=count;
     // }
-    // return tcnt; 
+    // return tcnt;
 
     virtual int_type   overflow(int_type c = traits::eof());
     // if eq_int_type(c, traits::eof())
     //      pending sequence:[pbase(),pptr)
-    // else 
+    // else
     //      pending sequence:[pbase(),pptr) + c
     // consume pending sequence;
-    // return traits_type::not_eof(ch);
+    // return traits_type::not_eof(c);
+
+public:
+    // 31.6.3.3.3, get area
+    streamsize in_avail();
+    // if read position is available
+    //      returns egptr() - gptr();
+    // otherwise
+    //      return showmanyc();
+
+    int_type sgetc();
+    // if read position is not available
+    //      return underflow();
+    // otherwise
+    //      return traits::to_int_type(*gptr());
+
+    int_type sbumpc();
+    // if read position is not available
+    //      return uflow();
+    // otherwise
+    //      int_type c = traits::to_int_type(*gptr());
+    //      if traits::eq_int_type(c, traits::eof()) {
+    //          return traits::eof();
+    //      }
+    //      gptr++;
+    //      return c;
+
+    int_type snextc();
+    // int_type c = sbumpc();
+    // if traits::eq_int_type(c, traits::eof())
+    //      return traits::eof();
+    // otherwise
+    //      return sgetc(); // next character
+
+
+    streamsize sgetn(char_type* s, streamsize n);
+    // return xsgetn(s, n);
+
+protected:
+    // 31.6.3.4.2, get area access[input]
+    char_type* eback() const;
+    char_type* gptr() const;
+    char_type* egptr() const;
+    void       gbump(int n);
+    // gptr() += n;
+    void       setg(char_type* gbeg, char_type* gnext, char_type* gend);
+
+    // 31.6.3.5.3, get area
+    virtual streamsize showmanyc();
+    // 양수: 즉시 읽어낼 수 있는 문자수
+    //   0: blocking할 수 있지만 여전히 읽어낼 문자가 있음.
+    //  -1: 읽어낼 문자가 없음
+    virtual int_type underflow();
+    // input seqence assign from pending sequence(usual backup condition 구현)
+    // if the pending sequence is empty,
+    //      return traits::eof();
+    // else
+    //      pending sequence 채움.
+    //      return traits::to_int_type(c), where c is the first character of the pending sequence.
+    // (peek mode)
+    virtual int_type uflow();
+    //  int_type c = underflow();
+    //  do {
+    //       if traits::eq_int_type(c, traits::eof()) break;
+    //       gbump(1);
+    //  } while(false);
+    //  return c;
+
+    virtual streamsize xsgetn(char_type* s, streamsize n);
+    //  streamsize tcnt{};
+    //  while(n>0){
+    //   if(gptr() >= egptr()) {
+    //      if traits::eq_int_type(underflow(), traits_type::eof()) break;
+    //   }
+    //   streamsize count = min(egptr() - gptr(),n);
+    //   memcpy(s,gptr(),count);
+    //   gbump(count);
+    //   tcnt += count;
+    //   n -= count
+    //   s += count;
+    // }
+    // return tcnt;
 };
 //*/
