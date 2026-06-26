f678f7edd705fdbd297b5271ccc6b4b965ce744d
sim9108 <sim9108@naver.com>
2026-06-26 16:14:05 +0900

    stringbuf, spanbuf, filebuf

---
+++ level003/026.cpp
@@ -0,0 +1,56 @@
+﻿import std;
+using namespace std;
+
+void ospan_test(span<char> data){
+    ospanstream os{ data };
+    os << "100 200 300";
+}
+
+void ispan_test(span<char const> cdata, span<int> vals) {
+    ispanstream is{ cdata };
+    for (auto& i : vals) {
+        is >> i;
+        if (is.fail()) break;
+    }
+    is.clear();
+
+   // char_T
+   // array<char, 7> data2 = { '3', '0', '0', ' ', '4', '0', '0' };
+   // string data2 { "300 400"};
+   // vector<char> data2{ '3', '0', '0', ' ', '4', '0', '0' };
+    string_view data2{ "300 400" };
+
+    is.span(data2);
+    int a{}, b{};
+    is >> a >> b;
+    println("a:{} b:{}", a, b);
+}
+
+void FourCC_test(span<char> data) {
+    do {
+        ofstream fo("test.txt");
+        fo << "H264 data";
+    } while (false);
+
+    ospanstream ss{ data };
+    do {
+        ifstream fi("test.txt");
+        ss << fi.rdbuf();
+    } while (false);
+    println("{:.{}}", data.data(), data.size());
+}
+
+auto main() -> int {
+    char buf[20]{}; // 최대 3개 숫자 포함 문자
+    int vals[3]{};  // 
+
+    char FCC[4]{};  // Four-Character Code
+
+    ospan_test(buf);
+    ispan_test(buf, vals);
+    FourCC_test(FCC);
+
+    println("out:{:.20}", buf);
+    println("vals:{}", vals);
+    return 0;
+}
---
+++ level003/027.cpp
@@ -0,0 +1,56 @@
+﻿import std;
+using namespace std;
+
+template<typename CharT, class _Statetype>
+struct formatter<fpos<_Statetype>, CharT> : formatter<streamsize, CharT> {
+    using BASE_TYPE = formatter<streamsize, CharT>;
+
+    auto format(const fpos<_Statetype>& p, auto& ctx) const {
+        return BASE_TYPE::format(static_cast<long long>(p), ctx);
+    }
+};
+
+void app_test() {
+    const string filename = "app_pos_test.txt";   
+    do{
+        std::ofstream init_file(filename, ios_base::trunc);
+        init_file << "0123456789";
+    } while (false);
+
+    do {
+        fstream file(filename, ios_base::in | ios_base::out | ios_base::app | ios_base::ate);
+        using char_type = fstream::char_type;
+
+        println("#1:{}", file.tellp());
+
+        file.seekp(0, ios_base::beg);
+        println("#2:{}", file.tellp());
+
+        file << "ABC";
+        println("#3:{}", file.tellp());
+
+        file.seekp(0, ios_base::beg);
+        println("#4:{}", file.tellp());
+
+        file << "abc";
+        println("#5:{}", file.tellp());
+
+        char_type fourCC[4]{};
+        file.seekp(0, ios_base::beg);
+        file >> fourCC;
+        println("fourCC:{:.4}", fourCC);
+
+    } while (false);
+
+    do {
+       ifstream check_file(filename);
+       ostringstream ss;
+       ss << check_file.rdbuf();
+       println("{}", ss.view());
+    } while (false);
+
+ }
+auto main() -> int {
+    app_test();
+    return 0;
+}
